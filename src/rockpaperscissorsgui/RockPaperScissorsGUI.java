fc29c819e625ad206e2740afec5b27f19a0c3d20
alex <aocarrol.o4l@gmail.com>
2020-12-10 14:23:40 +0000

    test

---
+++ src/rockpaperscissorsgui/RockPaperScissorsGUI.java
@@ -0,0 +1,25 @@
+
+package rockpaperscissorsgui;
+
+import java.awt.Toolkit;
+import java.util.Random;
+
+
+public class RockPaperScissorsGUI {
+    
+    static Random rand = new Random();
+    
+    public static void main(String[] args) {
+        
+        
+       Game game = new Game();
+       
+       Toolkit tk = Toolkit.getDefaultToolkit(); 
+       int xSize = (int)tk.getScreenSize().getWidth();//gets height and width of screen
+       int ySize = (int)tk.getScreenSize().getHeight();
+       game.setSize(xSize,ySize);//width, height
+       game.setVisible(true);
+    
+    }
+}
+    
